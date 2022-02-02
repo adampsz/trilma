ec75d3d07b0167fe3c7fed7f5754071135df95f7
Adam Szeruda <32493319+adampsz@users.noreply.github.com>
2022-02-03 00:57:24 +0100

    Add about page

--- server/routes/game.ts
+++ server/routes/game.ts
@@ -1,17 +1,21 @@
 import { Router } from "express";
 import error from "http-errors";
+
 import Game from "../game/Game";
-import type { Request, Response } from "express";
+
+import type { Request } from "express";
 
 const router = Router();
 
-router.get("/join", (req: Request, res: Response, next) => {
+router.get("/about", (req, res) => res.render("about"));
+
+router.get("/join", (req, res, next) => {
   if (!req.session.name) return next(new error.Unauthorized());
 
   res.render("join", { req, games: Game.waiting() });
 });
 
-router.post("/join", (req: Request, res: Response, next) => {
+router.post("/join", (req: Request, res, next) => {
   if (typeof req.body.name !== "string" || req.body.name.length > 16)
     return next(new error.BadRequest());
 
@@ -19,14 +23,14 @@ router.post("/join", (req: Request, res: Response, next) => {
   res.redirect("/game");
 });
 
-router.get("/join/:id", (req: Request, res: Response, next) => {
+router.get("/join/:id", (req, res, next) => {
   if (!Game.all.has(req.params.id)) return next(new error.NotFound());
 
   req.session.room = req.params.id;
   res.redirect("/game");
 });
 
-router.get("/game", (req: Request, res: Response, next) => {
+router.get("/game", (req, res, next) => {
   const game = Game.all.get(req.session.room || "");
 
   if (!game) return next(new error.NotFound());
